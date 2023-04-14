f712e3e3398d1913c89949d9e0af137001655bb2
b1ack0p <blackop@disroot.org>
2023-04-15 00:22:09 +0300

    edit

--- services.sh
+++ services.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 dbus-update-activation-environment --systemd --all
-systemctl --user start wallpaper-rotater.service # to be able to run wallpaper-rotator script it needs X so we create this service.sh to run in .config/lxsession/LXDE/autostart
+systemctl --user start wallpaper-rotator.service	# to be able to run wallpaper-rotator script it needs X so we create this service.sh to run in .config/lxsession/LXDE/autostart
 # systemctl --user start another.service
 
-# run "systemctl --user restart wallpaper-rotator.service" to start/restart the script.
-# "systemctl --user status wallpaper-rotator.service"  to check status.
+# run "systemctl --user start/stop/restart/status wallpaper-rotator.service" to start/stop/restart and check status of the service.
 # DO NOT USE "sudo" with "--user"
